417f2c69aedfe03bd10f6dba7148a8af6c4a98e6
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-25 09:26:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(res => {
+   return res.json()
+  })
+  .then(data => { 
+    return JSON.stringify(renderBooks(data));
+    // renderBooks(data) 
+  }, 0)
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
